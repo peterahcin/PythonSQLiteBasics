74383b0f2d6b6b55107c5045a2c15ee89404b38d
peterahcin <peter.ahcin@gmail.com>
2020-08-06 11:37:45 +0300

    Update connection_details.py

--- resources/connection_details.py
+++ resources/connection_details.py
@@ -1,7 +1,7 @@
 
 
-USER = 'envirodual'
-PSWD = '{=@9F@wbG?V8an"g'
-HOST = 'ls-091091a6ab5739cd48010ba9a096ebd5ec1bd10e.c7kzjq9czojk.eu-central-1.rds.amazonaws.com'
-ENVIRODUAL_DATA_BASE = 'elek'
+USER = ''
+PSWD = ''
+HOST = ''
+ENVIRODUAL_DATA_BASE = ''
 ADDRESS = 'postgresql://' + USER + ':' + PSWD + '@' + HOST + ENVIRODUAL_DATA_BASE
